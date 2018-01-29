b062cf381fa9080f42942639b84277542ad0d0f3
Anh <anhtnt@elightup.com>
2018-01-29 08:42:58 +0700

    Add test for each_connected API.

---
+++ tests/04-each-connected.php
@@ -0,0 +1,13 @@
+<?php
+add_action( 'mb_relationships_init', function () {
+	MB_Relationships_API::register( array(
+		'id'   => 'posts_to_pages',
+		'from' => 'post',
+		'to'   => 'page',
+	) );
+	MB_Relationships_API::register( array(
+		'id'   => 'posts_to_posts',
+		'from' => 'post',
+		'to'   => 'post',
+	) );
+} );
