c33a61dda14ed1cc762f1cd2ebdcbefa0b02d714
Truong Giang <truongwp@gmail.com>
2018-04-20 16:56:10 +0700

    Add sibling api for post

--- inc/query/class-mb-relationships-query-normalizer.php
+++ inc/query/class-mb-relationships-query-normalizer.php
@@ -32,15 +32,29 @@ class MB_Relationships_Query_Normalizer {
 	 * @param array $args Query arguments.
 	 */
 	public function normalize( &$args ) {
-		$direction         = isset( $args['from'] ) ? 'from' : 'to';
-		$args['direction'] = $direction;
-
+		$direction        = isset( $args['from'] ) ? 'from' : 'to';
 		$relationship     = $this->factory->get( $args['id'] );
 		$args['id_field'] = $relationship->get_db_field( $direction );
 
-		$items         = $args[ $direction ];
-		$items         = $this->get_ids( $items, $args['id_field'] );
-		$args['items'] = $items;
+		if ( ! empty( $args['sibling'] ) ) {
+			$args['exclude'] = $args[ $direction ];
+			$items = get_posts( array(
+				'relationship' => array(
+					'id'   => $args['id'],
+					'from' => $args[ $direction ],
+				),
+				'nopaging'     => true,
+				'fields'       => 'ids',
+			) );
+
+			$args['direction'] = 'from' === $direction ? 'to' : 'from';
+			$args['items'] = $items;
+		} else {
+			$args['direction'] = $direction;
+			$items         = $args[ $direction ];
+			$items         = $this->get_ids( $items, $args['id_field'] );
+			$args['items'] = $items;
+		}
 
 		unset( $args[ $direction ] );
 	}
