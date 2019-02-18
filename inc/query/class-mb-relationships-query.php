7fd35b60551d8fd25ce2b379c7a647bacf8999dc
anhtu91 <tudt@elightup.com>
2019-02-18 16:15:26 +0700

    query by multiple relationships vesion 2

--- inc/query/class-mb-relationships-query.php
+++ inc/query/class-mb-relationships-query.php
@@ -37,50 +37,27 @@ class MB_Relationships_Query {
 	 */
 	public function alter_clauses( &$clauses, $id_column, $pass_thru_order = FALSE ) {
 		global $wpdb;
-		$clauses = $this->handle_query_join( $clauses, $id_column, $pass_thru_order, $this->args );
-		$where   = $this->handle_query_where( $clauses, $this->args );
+		$this->handle_query_join( $clauses, $id_column, $pass_thru_order );
 
 		if ( ! isset( $this->args['relation'] ) && ! empty( $this->args['sibling'] ) ) {
-			$direction = $this->args['direction'];
-			$connected = 'from' === $direction ? 'to' : 'from';
-			$items     = array_map( 'absint', $this->args['items'] );
-			$ids       = implode( ',', $items );
-			$items     = "(
-				SELECT DISTINCT `{$connected}`
-				FROM {$wpdb->mb_relationships}
-				WHERE `type` = {$wpdb->prepare( '%s', $this->args['id'] )}
-				AND `{$direction}` IN ({$ids})
-			)";
-			$tmp       = $direction;
-			$direction = $connected;
-			$connected = $tmp;
-
-			$clauses['join'] = " INNER JOIN $wpdb->mb_relationships AS mbr ON mbr.$connected = $id_column";
-
-			$where = sprintf(
-				"mbr.type = %s AND mbr.$direction IN (%s)",
-				$wpdb->prepare( '%s', $this->args['id'] ),
-				is_array( $items ) ? implode( ',', $items ) : $items
-			);
-			$where .= " AND mbr.$connected NOT IN ($ids)";
+			$this->handle_query_sibling( $clauses, $id_column );
 		}
-		$clauses['where'] .= empty( $clauses['where'] ) ? $where : " AND $where";
 
 		return $clauses;
 	}
 
-	public function handle_query_join( &$clauses, $id_column, $pass_thru_order, $args ) {
+	public function handle_query_join( &$clauses, $id_column, $pass_thru_order ) {
 		global $wpdb;
 		$join_type = '';
 		$criteria  = '';
 		$query     = array();
 
-		if ( isset( $args['relation'] ) ) {
-			$join_type = $args['relation'];
-			unset( $args['relation'] );
-			$query     = $args;
+		if ( isset( $this->args['relation'] ) ) {
+			$join_type = $this->args['relation'];
+			unset( $this->args['relation'] );
+			$query     = $this->args;
 		} else {
-			$query[] = $args;
+			$query[] = $this->args;
 		}
 
 		foreach ( $query as $key => $value ) {
@@ -94,51 +71,47 @@ class MB_Relationships_Query {
 			}
 
 			if ( ! $pass_thru_order ) {
-				$orderby            = "mbr.order_$direction";
+				$orderby            = "mbr.order_$source";
 				$clauses['orderby'] = 't.term_id' === $id_column ? "ORDER BY $orderby" : $orderby;
 			}
 
-			$fields             = "mbr.$direction AS mb_origin";
+			$fields             = "mbr.$source AS mb_origin";
 			$clauses['fields'] .= empty( $clauses['fields'] ) ? $fields : " , $fields";
 
 			$criteria .= sprintf(
-				" (mbr.$target = $id_column AND mbr.type = %s AND mbr.$source = %s) ",
+				" (mbr.$target = $id_column AND mbr.type = %s AND mbr.$source IN (%s)) ",
 				$wpdb->prepare( '%s', $value['id'] ),
-				$items[0]
+				is_array( $items ) ? implode( ',', $items ) : $items
 			);
 		}
 
 		$clauses['join'] .= " INNER JOIN $wpdb->mb_relationships AS mbr ON $criteria";
-		return $clauses;
 	}
 
-	public function handle_query_where( &$clauses, $args ) {
+	public function handle_query_sibling( &$clauses, $id_column ) {
 		global $wpdb;
-		$where_direction  = '';
-		$query = $id_relationship = array();
+		$direction = $this->args['direction'];
+		$connected = 'from' === $direction ? 'to' : 'from';
+		$items     = array_map( 'absint', $this->args['items'] );
+		$ids       = implode( ',', $items );
+		$items     = "(
+			SELECT DISTINCT `{$connected}`
+			FROM {$wpdb->mb_relationships}
+			WHERE `type` = {$wpdb->prepare( '%s', $this->args['id'] )}
+			AND `{$direction}` IN ({$ids})
+		)";
+		$tmp       = $direction;
+		$direction = $connected;
+		$connected = $tmp;
 
-		if ( isset( $args['relation'] ) ) {
-			unset( $args['relation'] );
-			$query = $args;
-		} else {
-			$query[] = $args;
-		}
+		$clauses['join'] = " INNER JOIN $wpdb->mb_relationships AS mbr ON mbr.$connected = $id_column";
 
-		foreach ( $query as $key => $value ) {
-			$direction         = $value['direction'];
-			$items             = array_map( 'absint', $value['items'] );
-			$id_relationship[] =  "@@" . $value['id'] . "@@";
-
-			$where_direction .= sprintf(
-				" AND mbr.$direction IN (%s)",
-				is_array( $items ) ? implode( ',', $items ) : $items
-			);
-		}
-	 	$where = sprintf(
-			"mbr.type IN (%s)",
-			is_array( $id_relationship ) ? str_replace( "@@", "'", implode( ',', $id_relationship ) ) : str_replace( "@@", "'", $id_relationship )
+		$where = sprintf(
+			"mbr.type = %s AND mbr.$direction IN (%s)",
+			$wpdb->prepare( '%s', $this->args['id'] ),
+			is_array( $items ) ? implode( ',', $items ) : $items
 		);
-		$where .= $where_direction;
-		return $where;
+		$where .= " AND mbr.$connected NOT IN ($ids)";
+		$clauses['where'] .= empty( $clauses['where'] ) ? $where : " AND $where";
 	}
 }
