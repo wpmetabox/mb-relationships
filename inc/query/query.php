1bd64a157e1417f13847b5612ea0050b7bb3e772
Anh Tran <anhtnt@elightup.com>
2020-07-03 15:32:48 +0700

    Fix wrong table prefix

--- inc/query/query.php
+++ inc/query/query.php
@@ -75,7 +75,7 @@ class MBR_Query {
 		$items  = implode( ',', array_map( 'absint', $relationship['items'] ) );
 
 		if ( $relationship['reciprocal'] ) {
-			$fields             = 'mbr.from AS mbr_from, mbr.to AS mbr_to, mbr.ID AS mbr_id, CASE WHEN mbr.to = wp_posts.ID THEN mbr.order_from WHEN mbr.from = wp_posts.ID THEN mbr.order_to END AS `mbr_order`';
+			$fields             = "mbr.from AS mbr_from, mbr.to AS mbr_to, mbr.ID AS mbr_id, CASE WHEN mbr.to = {$wpdb->posts}.ID THEN mbr.order_from WHEN mbr.from = {$wpdb->posts}.ID THEN mbr.order_to END AS `mbr_order`";
 			$clauses['fields'] .= empty( $clauses['fields'] ) ? $fields : " , $fields";
 			$clauses['orderby'] = '`mbr_order` ASC, mbr_id DESC';
 			if ( empty( $clauses['groupby'] ) ) {
@@ -87,7 +87,7 @@ class MBR_Query {
 				$relationship['id'],
 				$items,
 				$items
-			);	
+			);
 		}
 
 		if ( ! $pass_thru_order ) {
