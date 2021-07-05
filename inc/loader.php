4fa9a512040fd2c83aee1036e48e2e1e6e8e11e1
Ngoc Anh <anhdoanmis@gmail.com>
2021-07-05 14:56:27 +0700

    Fix mb relationship create table

--- inc/loader.php
+++ inc/loader.php
@@ -10,19 +10,12 @@
  * The loader class.
  */
 class MBR_Loader {
-	/**
-	 * Detect if the relationships table is created.
-	 *
-	 * @var bool
-	 */
-	protected $is_table_created = false;
 
 	/**
 	 * Plugin activation.
 	 */
 	public function activate() {
 		$this->create_table();
-		$this->is_table_created = true;
 	}
 
 	/**
@@ -39,8 +32,10 @@ class MBR_Loader {
 		 * If plugin is embed in another plugin, the table is not created during activation.
 		 * Thus, we have to create it while initializing.
 		 */
-		if ( ! $this->is_table_created ) {
+		$is_table_created = get_option('mb_is_table_created');
+		if ( ! $is_table_created ) {
 			$this->create_table();
+			update_option('mb_is_table_created', 1);
 		}
 
 		$obj_factory = new MBR_Object_Factory();
