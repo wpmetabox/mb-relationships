f4a9d9f819bce37322464f36006aea3e543b1858
Ngoc Anh <anhdoanmis@gmail.com>
2021-07-13 14:47:43 +0700

    Fix Relatioships stop working

--- inc/relationship/admin-columns.php
+++ inc/relationship/admin-columns.php
@@ -234,7 +234,7 @@ class MBR_Admin_Columns {
 
 	private function get_post_items( $object_id, $direction ) {
 		$mbr = MB_Relationships_API::get_relationship($this->id);
-		$post_types = isset( $mbr->from ) && isset( $mbr->to ) && 'from' === $direction
+		$post_types = isset( $mbr->from ) && isset( $mbr->to )
 			? array( $mbr->from['field']['post_type'], $mbr->to['field']['post_type'] )
 			: 'any';
 		$query = new WP_Query( [
