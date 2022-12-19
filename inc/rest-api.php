8b04225d97d8fbfdc5b1c9860f3c461fad51fabd
Andrew Minion <andrew@andrewrminion.com>
2022-12-19 14:35:45 -0600

    add delete endpoint

--- inc/rest-api.php
+++ inc/rest-api.php
@@ -59,6 +59,17 @@ class MB_Relationships_REST_API {
 				'callback'            => array( $this, 'create_relationship' ),
 			)
 		);
+
+		register_rest_route(
+			self::NAMESPACE,
+			'/(?P<relationship>[a-zA-Z0-9-_]+)/',
+			array(
+				'methods'             => WP_REST_Server::DELETABLE,
+				'args'                => $this->relationship_args(),
+				'permission_callback' => array( $this, 'delete_relationship_permission' ),
+				'callback'            => array( $this, 'delete_relationship' ),
+			)
+		);
 	}
 
 	/**
@@ -231,6 +242,44 @@ class MB_Relationships_REST_API {
 	}
 
 	/**
+	 * Determine whether the current user has permission to use the delete_relationship endpoint.
+	 *
+	 * @return WP_Error|bool
+	 */
+	public function delete_relationship_permission() {
+
+		/**
+		 * Whether the REST API allows unauthenticated users to delete relationships.
+		 *
+		 * @param bool $allow_public_rest_api_delete Whether the REST API allows unauthenticated users to delete relationships.
+		 *
+		 * @return bool
+		 */
+		$allow_public_access = apply_filters( 'mb_relationships_rest_api_can_delete_relationships_public', false );
+
+		if ( $allow_public_access ) {
+			return true;
+		}
+
+		if ( 0 === get_current_user_id() ) {
+			return new WP_Error( 'rest-forbidden', __( 'You are not allowed to access this API endpoint.', 'mb-relationships' ), array( 'status' => 401 ) );
+		}
+
+		/**
+		 * Whether the REST API allows authenticated users to delete relationships.
+		 *
+		 * @param bool $allow_authenticated_user_create Whether the REST API allows authenticated users to create relationships.
+		 */
+		$permission = apply_filters( 'mb_relationships_rest_api_can_delete_relationships', 'delete_posts' );
+
+		if ( current_user_can( $permission ) ) {
+			return true;
+		}
+
+		return new WP_Error( 'rest-forbidden', __( 'You are not allowed to access this API endpoint.', 'mb-relationships' ), array( 'status' => 403 ) );
+	}
+
+	/**
 	 * Checks if the given from and to have a relationship for the given relationship ID.
 	 *
 	 * @param WP_REST_Request $request Request object.
@@ -290,6 +339,34 @@ class MB_Relationships_REST_API {
 		}
 
 		$added = MB_Relationships_API::add( $from, $to, $relationship, $order_from, $order_to );
+
+		return $this->generic_response( $from, $to, $relationship );
+	}
+
+	/**
+	 * Deletes a relationship.
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 *
+	 * @return WP_REST_Response|WP_Error Response object on success or WP_Error object on failure.
+	 */
+	public function delete_relationship( $request ) {
+		$relationship = $this->get_url_field_from_request( $request, 'relationship' );
+		if ( is_wp_error( $relationship ) ) {
+			return $relationship;
+		}
+
+		$to = $this->get_query_field_from_request( $request, 'to' );
+		if ( is_wp_error( $to ) ) {
+			return $to;
+		}
+
+		$from = $this->get_query_field_from_request( $request, 'from' );
+		if ( is_wp_error( $from ) ) {
+			return $from;
+		}
+
+		$deleted = MB_Relationships_API::delete( $from, $to, $relationship );
 
 		return $this->generic_response( $from, $to, $relationship );
 	}
