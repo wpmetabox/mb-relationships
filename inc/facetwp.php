14bb25372e582c4a1cfc3538cd53dc46f47d7111
Andrew Minion <andrew@andrewrminion.com>
2022-12-30 12:38:58 -0600

    index related objects

--- inc/facetwp.php
+++ inc/facetwp.php
@@ -7,13 +7,25 @@ class MB_Relationships_FacetWP extends FacetWP_Facet {
 	const FACET_TYPE = 'mb_relationships';
 
 	/**
+	 * FacetWP Indexer.
+	 *
+	 * @var FacetWP_Indexer
+	 */
+	protected $indexer;
+
+	/**
 	 * Construct the class.
 	 *
 	 * @since 1.12.0
 	 */
 	public function __construct() {
 		$this->label = __( 'MB Relationships', 'mb-relationships' );
+
+		// Add all registered relationships as FacetWP sources.
 		add_filter( 'facetwp_facet_sources', [ $this, 'facet_sources' ] );
+
+		// Hook into indexer.
+		add_filter( 'facetwp_indexer_post_facet', [ $this, 'facetwp_indexer_post_facet' ], 10, 2 );
 	}
 
 	/**
@@ -43,5 +55,84 @@ class MB_Relationships_FacetWP extends FacetWP_Facet {
 		}
 
 		return $sources;
+	}
+
+	/**
+	 * Index MB relationships.
+	 *
+	 * @since 1.12.0
+	 *
+	 * @param bool  $bypass Bypass default indexing.
+	 * @param array $params Extra helper data.
+	 *
+	 * @return array
+	 */
+	public function facetwp_indexer_post_facet( $bypass, $params ) {
+		if ( ! isset( $params['facet']['source'] ) || self::FACET_TYPE . '/' !== substr( $params['facet']['source'], 0, 17 ) ) {
+			return $bypass;
+		}
+
+		$this->indexer   = FWP()->indexer;
+		$relationship_id = substr( $params['facet']['source'], 17 );
+
+		$connected_objects = MB_Relationships_API::get_connected([
+			'id' => $relationship_id,
+			'from' => $params['defaults']['post_id'],
+		]);
+
+		// If no related objects, stop processing.
+		if ( empty( $connected_objects ) ) {
+			return true;
+		}
+
+		foreach ( $connected_objects as $connected_object ) {
+			$this->index_field_value( $connected_object, $params['defaults'] );
+		}
+
+		return $bypass;
+	}
+
+	/**
+	 * Manually index a relationship value.
+	 *
+	 * @since 1.12.0
+	 *
+	 * @param WP_Post|WP_Term|WP_User $object Connected object.
+	 * @param array                   $params Extra helper data.
+	 *
+	 * @return void
+	 */
+	protected function index_field_value( $object, $params ) {
+		switch ( get_class( $object ) ) {
+			case WP_Term::class:
+				$params['facet_value']         = $object->term_id;
+				$params['facet_display_value'] = $object->name;
+				break;
+
+			case WP_User::class:
+				$params['facet_value']         = $object->ID;
+				$params['facet_display_value'] = $object->display_name;
+				break;
+
+			case WP_Post::class:
+			default:
+				$params['facet_value']         = $object->ID;
+				$params['facet_display_value'] = get_the_title( $object );
+				break;
+		}
+
+		/**
+		 * Filters the FacetWP data for a connected object.
+		 *
+		 * @since 1.12.0
+		 *
+		 * @param array $params FacetWP object params.
+		 * @param mixed $object Connected object.
+		 *
+		 * @return array
+		 */
+		$params = apply_filters( 'mb-relationships-facet-index-value', $params, $object );
+
+		$this->indexer->index_row( $params );
 	}
 }
