683557ee0a3ae0482849af084cc30b20002dc066
Anh Tran <anhtnt@elightup.com>
2025-07-04 11:05:56 +0700

    Simplify code

--- js/admin-filter.js
+++ js/admin-filter.js
@@ -1,4 +1,4 @@
-( function ( $, document ) {
+( function ( $ ) {
 	'use strict';
 
 	/**
@@ -7,37 +7,27 @@
 	function transform() {
 		const $this = $( this );
 
-		let options = {
+		const options = {
 			allowClear: true,
+			minimumInputLength: 1,
+			ajax: {
+				url: ajaxurl,
+				dataType: 'json',
+				delay: 250,
+				cache: true,
+				data: params => ( {
+					q: params.term,
+					action: 'mbr_admin_filter',
+					_ajax_nonce: MBR.nonce,
+					object_type: $this.data( 'object_type' ),
+					type: $this.data( 'type' ),
+				} ),
+				processResults: response => ( { results: response.data } ),
+			}
 		};
-		// the minimum of symbols to input before perform a search
-		options.minimumInputLength = 1;
-
-		options.ajax = {
-			url: ajaxurl,
-			dataType: 'json',
-			delay: 250,
-			cache: true
-		};
-
-		options.ajax.data = function ( params ) {
-			return {
-				q: params.term,
-				action: 'mbr_admin_filter',
-				_ajax_nonce: MBR.nonce,
-				object_type: $this.data( 'object_type' ),
-				type: $this.data( 'type' ),
-			};
-		};
-
-		options.ajax.processResults = response => ( { results: response.data } );
 
 		$this.select2( options );
 	}
 
-	function init() {
-		$( '.mb_related_filter' ).each( transform );
-	}
-
-	$( document ).ready( init );
+	$( () => $( '.mb_related_filter' ).each( transform ) );
 } )( jQuery );
